7df1fbb222e0199cd5cdf12f4a6c44429483c995
zjzhao1002 <98807606+zjzhao1002@users.noreply.github.com>
2025-05-16 08:41:25 +0200

    Update pdf-rag.py

--- pdf-rag.py
+++ pdf-rag.py
@@ -4,7 +4,6 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain.prompts import PromptTemplate
-from langchain.chains.question_answering import load_qa_chain
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
